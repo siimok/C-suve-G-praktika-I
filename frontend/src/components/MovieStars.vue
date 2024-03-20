4b2b86c80e02c924ed794912990ad9c354f6943f
Siimo <siimokangruoja@hotmail.com>
2024-03-20 08:53:55 +0200

    Update star color;

--- frontend/src/components/MovieStars.vue
+++ frontend/src/components/MovieStars.vue
@@ -15,14 +15,14 @@ const halfStar = computed(() => (Math.round(props.count * 2) / 2) % 1)
     <svg
       v-for="n in fullStars"
       :key="n"
-      xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor"
+      xmlns="http://www.w3.org/2000/svg" fill="#F5B318" viewBox="0 0 24 24" stroke-width="0" stroke="currentColor"
       class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round"
             d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
     </svg>
 
     <svg v-if="halfStar"
-         xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 1 24" stroke-width="1" stroke="currentColor"
+         xmlns="http://www.w3.org/2000/svg" fill="#F5B318" viewBox="0 0 1 24" stroke-width="0" stroke="currentColor"
          class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round"
             d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
