bc970959759d64f546c240e2c5eb3d1175cbdcd1
Siimo <siimokangruoja@hotmail.com>
2024-03-20 09:04:53 +0200

    Style Movie list and cards;

--- frontend/src/components/MovieCard.vue
+++ frontend/src/components/MovieCard.vue
@@ -9,26 +9,38 @@ const props = defineProps({ movie: { type: Object as PropType<Movie> } })
 </script>
 
 <template>
-  <div class="flex flex-auto relative border border-gray-200 rounded-lg shadow hover:bg-gray-100 w-full h-64 m-4">
-    <div class="h-100 w-1/2 bg-gray-100">Image placeholder</div>
-    <div class="w-1/2 p-4">
-      <h2 class="h-16 text-center font-bold text-2xl capitalize overflow-ellipsis overflow-hidden">
-        {{ movie?.name }}
-      </h2>
+    <div class="flex flex-auto relative border border-gray-200 rounded-lg shadow hover:scale-105 w-full h-64">
+      <div class="h-100 w-1/2 bg-gray-100">Image placeholder</div>
+      <div class="w-1/2 p-4">
+        <div>
+          <h2 class="h-16 text-center font-bold text-2xl capitalize overflow-ellipsis overflow-hidden">
+            {{ movie?.name }}
+          </h2>
 
-      <movie-stars count:movie.rating />
+          <movie-stars count:movie.rating />
 
-      <h3 class="capitalize flex justify-between text-right mt-2">
-        <p>Language: {{ movie?.language ? movie?.language : 'Estonian' }}</p>
-        <p>{{ movie?.published }}</p>
-      </h3>
+          <h3 class="capitalize flex justify-between text-right mt-3">
+            <p>Language: {{ movie?.language ? movie?.language : 'Estonian' }}</p>
+            <p>{{ movie?.published }}</p>
+          </h3>
 
-      <p class="h-[4.5rem] capitalize text-ellipsis overflow-hidden">
-        {{ movie?.description }}
-      </p>
-      {{ props }}
+          <p class="h-[4.5rem] capitalize text-ellipsis overflow-hidden">
+            {{ movie?.description }}
+          </p>
+        </div>
+
+        <div class="mt-auto flex">
+          <div
+            v-for="(genre, index) in movie?.genres"
+            :key="index"
+            class="px-3 py-1   mr-2 border rounded-full"
+          >
+            {{ genre.name }}
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+
 </template>
 
 <style scoped>
--- frontend/src/components/MovieList.vue
+++ frontend/src/components/MovieList.vue
@@ -7,7 +7,7 @@ const props = defineProps({ movies: { type: Object as PropType<Array<Movie>> } }
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 justify-between">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 gap-4 justify-between">
     <div
       v-for="movie in movies"
       :key="movie.id"
