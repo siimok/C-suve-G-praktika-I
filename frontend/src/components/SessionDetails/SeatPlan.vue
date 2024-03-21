4f1cb17c980b8b2e1cdb6c4c490e058ee80cb9bb
Siimo <siimokangruoja@hotmail.com>
2024-03-21 16:41:25 +0200

    Improve seating algorithm;

--- frontend/src/components/SessionDetails/SeatPlan.vue
+++ frontend/src/components/SessionDetails/SeatPlan.vue
@@ -5,9 +5,11 @@ import type { Ticket } from '@/types/types'
 
 const props = defineProps({ tickets: { type: Object as PropType<Array<Ticket>>, required: true } })
 
+const partySize = ref(1)
+
 const alreadyBooked = computed(() => {
   return props.tickets.map(ticket => {
-    return parseInt(ticket.rowNumber) * 10 + parseInt(ticket.seatNumber)
+    return parseInt(ticket.rowNumber + 1) * 10 + parseInt(ticket.seatNumber + 1)
   })
 })
 
@@ -21,35 +23,12 @@ function selectSeat(seat: number) {
   }
 }
 
-
-//Cinema size is fixed, lets just order all seats by their rating.
-let seatRatingClasses: number[][];
-
-(function() {
-  const result = Array.from({ length: 13 }, () => [])
-
-  for (let seatNo = 1; seatNo <= 70; seatNo++) {
-    let score = 0
-
-    score += (Math.abs(Math.floor(seatNo / 10) - 4)) * 2  // Row difference has weight of 2
-    score += Math.abs(seatNo % 10 - 5) // seat difference has weight of 1
-
-    result[score].push(seatNo)
-  }
-
-  seatRatingClasses = result  // Assign the result to seatRatingClasses
-})()
-
-
 //Home-brewed algorithm.
 //First it downgrades seating plan into a string and groupsize into a substring of free seats.
 //Per row tries to find most optimal place.
 function findBestSeats(groupSize: number) {
-  let seatingPlan = []
+  let seatingPlan = Array.from({ length: 7 }, () => '0000000000')
 
-  for (let i = 0; i < 7; i++) {
-    seatingPlan.push('0000000000')
-  }
   const groupString = '0'.repeat(groupSize)
 
   //mark booked seats.
@@ -62,22 +41,44 @@ function findBestSeats(groupSize: number) {
   let bestScore = 100
   let bestStartingPosition
 
-  //Mark optimal start place (needed if more than one seat is generated)
-  let optimalStartSeat = Math.ceil(10 / groupSize)
+  //Optimal place to start seating the party
+  let optimalStartSeat = 6 - Math.ceil(groupSize / 2)
   let optimalRow = 4
 
   for (let i = 0; i < 7; i++) {
     let currentScore = 0
+
     let availableStartingPositions = findAllIndexes(seatingPlan[i], groupString)
+
+    //continue if party wont fit into current row
+    if (availableStartingPositions.length < 1) continue
     let bestAvailableStartSeat = availableStartingPositions.reduce((closest, current) => {
-      return Math.abs(current - optimalStartSeat) < Math.abs(closest - optimalStartSeat) ? current : closest
+      return Math.abs(current - optimalStartSeat) < Math.abs(closest - optimalStartSeat) ?
+        current : closest
     })
+
     currentScore = Math.abs(bestAvailableStartSeat - optimalStartSeat) + Math.abs(i - optimalRow) * 2
+    console.log('Score for row: ' + i + ' is ' + currentScore)
+    console.log('Row penalty: ' + Math.abs(bestAvailableStartSeat - optimalStartSeat) + ' Seat penalty: ' + Math.abs(i - optimalRow) * 2)
     if (bestScore > currentScore) {
       bestScore = currentScore
       bestStartingPosition = i * 10 + bestAvailableStartSeat
     }
   }
+
+  if (bestStartingPosition) {
+    let bestSeats = []
+
+    for (let i = 0; i < groupSize; i++) {
+      bestSeats.push(bestStartingPosition + i)
+    }
+
+    selectedSeats.value = bestSeats
+
+  }
+
+
+  return bestStartingPosition
 }
 
 function replaceCharAtIndex(str: string, index: number) {
@@ -93,7 +94,7 @@ function findAllIndexes(str: string, substring: string) {
   let indexes = []
   let index = -1
   while ((index = str.indexOf(substring, index + 1)) !== -1) {
-    indexes.push(index)
+    indexes.push(index + 1)
   }
   return indexes
 }
@@ -103,8 +104,10 @@ function findAllIndexes(str: string, substring: string) {
 <template>
   <div>
     {{ alreadyBooked }}
+    {{ selectedSeats }}
     <div class="flex">
       <input
+        v-model="partySize"
         class="border mr-5 w-24 h-10"
         type="number"
         id="tentacles"
@@ -112,7 +115,10 @@ function findAllIndexes(str: string, substring: string) {
         min="1"
         max="10"
       />
-      <button @click="console.log(findBestSeats(3))">Generate seats</button>
+      <button
+        @click="console.log(findBestSeats(partySize))"
+      >Generate seats
+      </button>
     </div>
   </div>
 
@@ -126,10 +132,11 @@ function findAllIndexes(str: string, substring: string) {
         v-for="j in 10"
         :key="j"
         class="rounded-b mr-1 mb-3 rounded-lg w-6 h-6 "
-        :class="[
-          alreadyBooked.includes(i * 10 + j) ? 'bg-red-500' : 'bg-gray-100',
-          selectedSeats.includes(i * 10 + j) ? 'bg-blue-500' : ''
-        ]"
+        :class="{
+          'bg-red-500': alreadyBooked.includes(i * 10 + j),
+          'bg-blue-500': selectedSeats.includes(i * 10 + j),
+          'bg-gray-100': !alreadyBooked.includes(i * 10 + j) && !selectedSeats.includes(i * 10 + j)
+        }"
         :disabled="alreadyBooked.includes(i * 10 + j)"
         @click="selectSeat(i * 10 + j)"
       >
